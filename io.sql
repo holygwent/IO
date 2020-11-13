85aae717e7c86491049a6ec25f34327a910ae956
holygwent <39002523+holygwent@users.noreply.github.com>
2020-11-13 13:51:49 +0100

    Update io.sql
    Added pracownicy table

--- io.sql
+++ io.sql
@@ -2,7 +2,7 @@ CREATE TABLE Administrator(
 	[Login] [nvarchar](30)  NULL,
 	[Haslo] [nvarchar](30)  NULL
 );
-		 CREATE TABLE Klienci(
+CREATE TABLE Klienci(
 	[IDKlienta] [int]  NOT NULL,
 	[Imie] [nvarchar](30)  NULL,
 	[Nazwisko] [nvarchar](30)  NULL,
@@ -17,5 +17,21 @@ CREATE TABLE Administrator(
 	[Karnet] [int] NULL,
 	[DataWygasniecia] [datetime]  NULL,
 	PRIMARY KEY(IDKlienta)
+	);
+CREATE TABLE Pracownicy(
+	[IDPracownik] [int] NOT NULL,
+	[Imie] [nvarchar](30)  NULL,
+	[Nazwisko] [nvarchar](30)  NULL,
+	[Adres] [nvarchar](60)  NULL,
+	[Miasto] [nvarchar](20)  NULL,
+	[KodPocztowy] [nvarchar](10)  NULL,
+	[Kraj] [nvarchar](20)  NULL,
+	[Email] [nvarchar](40)  NULL,
+	[Telefon] [nvarchar](24)  NULL,
+	[Pesel] [nvarchar](11)  NULL,
+	[DataUrodzenia] [datetime]  NULL,
+	[DataZatrudnienia] [datetime]  NULL,
+	[NrBankowy] [nvarchar](40)  NULL,
+	PRIMARY KEY(IDPracownik)
 	);
 
