fc5ea1982434aee97049d8c398d5e44b5f18169a
holygwent <39002523+holygwent@users.noreply.github.com>
2020-11-13 13:51:09 +0100

    Update io.sql
    added klient table

--- io.sql
+++ io.sql
@@ -2,3 +2,20 @@ CREATE TABLE Administrator(
 	[Login] [nvarchar](30)  NULL,
 	[Haslo] [nvarchar](30)  NULL
 );
+		 CREATE TABLE Klienci(
+	[IDKlienta] [int]  NOT NULL,
+	[Imie] [nvarchar](30)  NULL,
+	[Nazwisko] [nvarchar](30)  NULL,
+	[Adres] [nvarchar](60)  NULL,
+	[Miasto] [nvarchar](20)  NULL,
+	[KodPocztowy] [nvarchar](10)  NULL,
+	[Kraj] [nvarchar](20)  NULL,
+	[Email] [nvarchar](40)  NULL,
+	[Telefon] [nvarchar](24)  NULL,
+	[IDZajecia] [int]  NULL,
+	[DataDoladowania] [datetime]  NULL,
+	[Karnet] [int] NULL,
+	[DataWygasniecia] [datetime]  NULL,
+	PRIMARY KEY(IDKlienta)
+	);
+
