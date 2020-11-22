af74398b6e4b98b3b9010bb41ef64a722def0eef
holygwent <39002523+holygwent@users.noreply.github.com>
2020-11-22 12:02:20 +0100

    cofnąłem zmiany

--- Tables.sql
+++ Tables.sql
@@ -2,12 +2,20 @@ CREATE TABLE Administrator(
 	[Login] [nvarchar](30)  NULL,
 	[Haslo] [nvarchar](30)  NULL
 );
+CREATE TABLE Administrator(
+	[Login] [nvarchar](30)  NULL,
+	[Haslo] [nvarchar](30)  NULL
+);
 CREATE TABLE Klienci(
 	[IDKlienta] [int]  NOT NULL,
 	[Imie] [nvarchar](30)  NULL,
 	[Nazwisko] [nvarchar](30)  NULL,
+	[Adres] [nvarchar](60)  NULL,
+	[Miasto] [nvarchar](20)  NULL,
+	[KodPocztowy] [nvarchar](10)  NULL,
+	[Kraj] [nvarchar](20)  NULL,
 	[Email] [nvarchar](40)  NULL,
-	[Haslo] [nvarchar](40) NULL,
+	[Telefon] [nvarchar](24)  NULL,
 	[IDZajecia] [int]  NULL,
 	[DataDoladowania] [datetime]  NULL,
 	[Karnet] [int] NULL,
@@ -18,16 +26,33 @@ CREATE TABLE Pracownicy(
 	[IDPracownik] [int] NOT NULL,
 	[Imie] [nvarchar](30)  NULL,
 	[Nazwisko] [nvarchar](30)  NULL,
+	[Adres] [nvarchar](60)  NULL,
+	[Miasto] [nvarchar](20)  NULL,
+	[KodPocztowy] [nvarchar](10)  NULL,
+	[Kraj] [nvarchar](20)  NULL,
 	[Email] [nvarchar](40)  NULL,
-	[Haslo] [nvarchar](40) NULL,
+	[Telefon] [nvarchar](24)  NULL,
+	[Pesel] [nvarchar](11)  NULL,
+	[DataUrodzenia] [datetime]  NULL,
+	[DataZatrudnienia] [datetime]  NULL,
+	[NrBankowy] [nvarchar](40)  NULL,
 	PRIMARY KEY(IDPracownik)
 	);
 CREATE TABLE Trenerzy(
 	[IDTrener] [int] NOT NULL,
 	[Imie] [nvarchar](30)  NULL,
 	[Nazwisko] [nvarchar](30)  NULL,
+	[Adres] [nvarchar](60)  NULL,
+	[Miasto] [nvarchar](20)  NULL,
+	[KodPocztowy] [nvarchar](10)  NULL,
+	[Kraj] [nvarchar](20)  NULL,
 	[Email] [nvarchar](40) NULL,
+	[Telefon] [nvarchar](24)  NULL,
+	[Pesel] [nvarchar](11)  NULL,
+	[DataUrodzenia] [datetime]  NULL,
+	[DataZatrudnienia] [datetime]  NULL,
 	[IDZajecia] [int] NOT NULL,
+	[NrBankowy] [nvarchar](40)  NULL,
 	PRIMARY KEY(IDTrener)
 	);
 CREATE TABLE Zajecia(
