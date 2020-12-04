203feb57ac32eaae963f01f0e0834706fa43faf8
holygwent <39002523+holygwent@users.noreply.github.com>
2020-12-04 10:53:22 +0100

    Stworzenie projektu

---
+++ IOApplication/IOApplication/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IOApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //do skopiowania usun jak skonczysz
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+      
+    }
+}
