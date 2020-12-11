56a0f154f4505ff224fecca13dc02b3e6d2def40
holygwent <39002523+holygwent@users.noreply.github.com>
2020-12-11 08:47:59 +0100

    automatyczne przypisywanie daty zarejestrowania oraz liczenie daty wygaśniecia

--- IOApplication/IOApplication/Controllers/KlientController.cs
+++ IOApplication/IOApplication/Controllers/KlientController.cs
@@ -53,10 +53,10 @@ namespace IOApplication.Controllers
             if (ModelState.IsValid)
             {
                 db.Klient.Add(klient);
-                if(klient.DataWygasniecia == null )
-                {
-                    klient.DataWygasniecia = klient.DataDoladowania.AddDays(klient.Karnet);
-                }
+
+                klient.DataDoladowania = DateTime.Now;
+                klient.DataWygasniecia = klient.DataDoladowania.AddDays(klient.Karnet);
+                
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
