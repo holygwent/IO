00022534ed8d35b020740888d0b3d4447b54234e
holygwent <39002523+holygwent@users.noreply.github.com>
2020-12-26 12:15:05 +0100

    stworzenie tablicy promocji oraz wypisanie promocji na stronie

--- IOApplication/IOApplication/Controllers/HomeController.cs
+++ IOApplication/IOApplication/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿
+using IOApplication.Models.CennikModel;
 using IOApplication.Models.CennikModel.Component;
 using IOApplication.Models.CennikModel.ConcreteComponent;
 using IOApplication.Models.CennikModel.ConcreteKarnetDecorator;
 using IOApplication.Models.CennikModel.ConcreteZajeciaDecorator;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,15 +17,34 @@ namespace IOApplication.Controllers
     {
         public ActionResult Index()
         {
+          
             Cennik promocja = new ConcreteComponent();
             promocja = new Days180KarnetDecorator(promocja);
             promocja = new JogaDecorator(promocja);
             promocja = new BoksDecorator(promocja);
             promocja = new FitnessDecorator(promocja);
-           
 
-            return View(promocja);
+            Cennik promocja2 = new ConcreteComponent();
+            promocja2 = new Days360KarnetDecorator(promocja2);
+            promocja2 = new TaniecDecorator(promocja2);
+            promocja2 = new FitnessDecorator(promocja2);
+
+            Cennik[] cenniki = new Cennik[2];
+            cenniki[0] = promocja;
+            cenniki[1] = promocja2;
+            
+            var tab = new TablicaCennikow();
+
+            tab.tablicaCennikow = cenniki;
+
+            return View(tab);
         }
+
+        private object TablicaCennikow()
+        {
+            throw new NotImplementedException();
+        }
+
         //do skopiowania usun jak skonczysz
         public ActionResult About()
         {
---
+++ IOApplication/IOApplication/Models/CennikModel/TablicaCennikow.cs
@@ -0,0 +1,13 @@
+﻿using IOApplication.Models.CennikModel.Component;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IOApplication.Models.CennikModel
+{
+    public class TablicaCennikow
+    {
+        public Cennik[] tablicaCennikow;
+    }
+}
