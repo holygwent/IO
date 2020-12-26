b98be9412bc3753e00dd4ba0145d30f9317db8b1
holygwent <39002523+holygwent@users.noreply.github.com>
2020-12-26 13:59:01 +0100

    update

--- io.sql
+++ io.sql
@@ -2,61 +2,72 @@ CREATE TABLE Administrator(
 	[Login] [nvarchar](30) NOT NULL,
 	[Haslo] [nvarchar](30) NOT NULL
 );
-CREATE TABLE Klienci(
-	[IDKlienta] [int]  NOT NULL,
-	[Imie] [nvarchar](30) NOT NULL,
-	[Nazwisko] [nvarchar](30) NOT NULL,
-	[Adres] [nvarchar](60) NOT NULL,
-	[Miasto] [nvarchar](20) NOT NULL,
-	[KodPocztowy] [nvarchar](10) NOT NULL,
-	[Kraj] [nvarchar](20) NOT NULL,
-	[Email] [nvarchar](40) NOT NULL,
-	[Telefon] [nvarchar](24) NOT NULL,
-	[IDZajecia] [int] NOT NULL,
-	[DataDoladowania] [datetime] NOT NULL,
-	[Karnet] [int] NOT NULL,
-	[DataWygasniecia] [datetime] NULL,
-	PRIMARY KEY(IDKlienta)
-	);
-CREATE TABLE Pracownicy(
-	[IDPracownik] [int] NOT NULL,
-	[Imie] [nvarchar](30) NOT NULL,
-	[Nazwisko] [nvarchar](30) NOT NULL,
-	[Adres] [nvarchar](60) NOT NULL,
-	[Miasto] [nvarchar](20) NOT NULL,
-	[KodPocztowy] [nvarchar](10) NOT NULL,
-	[Kraj] [nvarchar](20) NOT NULL,
-	[Email] [nvarchar](40) NOT NULL,
-	[Telefon] [nvarchar](24) NOT NULL,
-	[Pesel] [nvarchar](11) NOT NULL,
-	[DataUrodzenia] [datetime] NOT NULL,
-	[DataZatrudnienia] [datetime] NOT NULL,
-	[NrBankowy] [nvarchar](40) NOT NULL,
-	PRIMARY KEY(IDPracownik)
-	);
-CREATE TABLE Trenerzy(
-	[IDTrener] [int] NOT NULL,
-	[Imie] [nvarchar](30) NOT NULL,
-	[Nazwisko] [nvarchar](30) NOT  NULL,
-	[Adres] [nvarchar](60) NOT NULL,
-	[Miasto] [nvarchar](20) NOT NULL,
-	[KodPocztowy] [nvarchar](10) NOT NULL,
-	[Kraj] [nvarchar](20) NOT NULL,
-	[Email] [nvarchar](40) NOT NULL,
-	[Telefon] [nvarchar](24) NOT  NULL,
-	[Pesel] [nvarchar](11) NOT NULL,
-	[DataUrodzenia] [datetime] NOT NULL,
-	[DataZatrudnienia] [datetime] NOT NULL,
-	[IDZajecia] [int] NOT NULL,
-	[NrBankowy] [nvarchar](40) NOT NULL,
-	PRIMARY KEY(IDTrener)
-	);
-CREATE TABLE Zajecia(
-	[IDZajecia] [int] NOT NULL,
-	[Nazwa] [nvarchar](30) NOT NULL,
-	[Godzina] [time](7) NOT NULL,
-	PRIMARY KEY(IDZajecia)
-	); 
+CREATE TABLE [dbo].[Klient] (
+    [IdKlienta]       INT            IDENTITY (1, 1) NOT NULL,
+    [Imie]            NVARCHAR (50)  NOT NULL,
+    [Nazwisko]        NVARCHAR (50)  NOT NULL,
+    [Adres]           NVARCHAR (100) NOT NULL,
+    [Miasto]          NVARCHAR (50)  NOT NULL,
+    [KodPocztowy]     NVARCHAR (10)  NOT NULL,
+    [Kraj]            NVARCHAR (50)  NOT NULL,
+    [Telefon]         NVARCHAR (30)  NOT NULL,
+    [Email]           NVARCHAR (100) NOT NULL,
+    [DataDoladowania] DATE           NOT NULL,
+    [Karnet]          INT            NOT NULL,
+    [DataWygasniecia] DATE           NULL,
+    [IdZajecia]       INT            NOT NULL,
+    [Login]           NVARCHAR (50)  NOT NULL,
+    [Haslo]           NVARCHAR (50)  NULL,
+    PRIMARY KEY CLUSTERED ([IdKlienta] ASC),
+    CONSTRAINT [FK_Klient_ToKarnet] FOREIGN KEY ([Karnet]) REFERENCES [dbo].[Karnet] ([Id]),
+    CONSTRAINT [FK_Klient_ToZajecia] FOREIGN KEY ([IdZajecia]) REFERENCES [dbo].[Zajecia] ([IdZajecia])
+);
+CREATE TABLE [dbo].[Pracownicy] (
+    [IdPracownika]     INT            IDENTITY (1, 1) NOT NULL,
+    [Imie]             NVARCHAR (50)  NOT NULL,
+    [Nazwisko]         NVARCHAR (50)  NOT NULL,
+    [Adres]            NVARCHAR (100) NOT NULL,
+    [Miasto]           NVARCHAR (50)  NOT NULL,
+    [KodPocztowy]      NVARCHAR (30)  NOT NULL,
+    [Kraj]             NVARCHAR (50)  NOT NULL,
+    [Email]            NVARCHAR (100) NOT NULL,
+    [Telefon]          NVARCHAR (30)  NOT NULL,
+    [Pesel]            NVARCHAR (11)  NOT NULL,
+    [DataUrodzenia]    DATE           NOT NULL,
+    [DataZatrudnienia] DATE           NOT NULL,
+    [NrBankowy]        NVARCHAR (50)  NOT NULL,
+    [Login]            NVARCHAR (50)  NOT NULL,
+    [Haslo]            NVARCHAR (50)  NULL,
+    PRIMARY KEY CLUSTERED ([IdPracownika] ASC)
+);
+
+CREATE TABLE [dbo].[Trener] (
+    [IdTrener]         INT            IDENTITY (1, 1) NOT NULL,
+    [Imie]             NVARCHAR (50)  NOT NULL,
+    [Nazwisko]         NVARCHAR (50)  NOT NULL,
+    [Adres]            NVARCHAR (100) NOT NULL,
+    [Miasto]           NVARCHAR (50)  NOT NULL,
+    [KodPocztowy]      NVARCHAR (10)  NOT NULL,
+    [Kraj]             NVARCHAR (50)  NOT NULL,
+    [Email]            NVARCHAR (100) NOT NULL,
+    [Telefon]          NVARCHAR (30)  NOT NULL,
+    [Pesel]            NVARCHAR (11)  NOT NULL,
+    [DataUrodzenia]    DATE           NOT NULL,
+    [DataZatrudnienia] DATE           NOT NULL,
+    [IdZajecia]        INT            NOT NULL,
+    [NrBankowy]        NVARCHAR (50)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([IdTrener] ASC),
+    CONSTRAINT [FK_Trener_ToZajecia] FOREIGN KEY ([IdZajecia]) REFERENCES [dbo].[Zajecia] ([IdZajecia])
+);
+
+CREATE TABLE [dbo].[Zajecia] (
+    [IdZajecia] INT           IDENTITY (1, 1) NOT NULL,
+    [Nazwa]     NVARCHAR (50) NOT NULL,
+    [Godzina]   TIME (7)      NOT NULL,
+    PRIMARY KEY CLUSTERED ([IdZajecia] ASC)
+);
+
+
 CREATE TABLE [dbo].[Karnet] (
     [Id]    INT NOT NULL,
     [Dni]   INT NOT NULL,
