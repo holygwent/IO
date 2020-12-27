3570ad946d4f6e2de863f3943027bc4e083ddf46
holygwent <39002523+holygwent@users.noreply.github.com>
2020-12-27 17:56:38 +0100

    dodanie singletonu
    tworzenie obiektu ktory ma liste jest singletonem dzieki temu mamy pewnosc ze bedzie 1 obiekt i bedzie miał pełną liste.

---
+++ IOApplication/IOApplication/Models/CennikModel/ListaCennikow/ListaCennikow.cs
@@ -0,0 +1,33 @@
+﻿using IOApplication.Models.CennikModel.Component;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IOApplication.Models.CennikModel.ListaCennikow
+{
+    //singleton!!!!
+    public class ListaCennikow
+    {
+        private List<Cennik> listaCennikow = new List<Cennik>();
+        public int counter = 0;
+        private static ListaCennikow instance = null;
+        public static ListaCennikow GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ListaCennikow();
+                return instance;
+            }
+        }
+        public void DodajDoListy(Cennik c)
+        {
+            listaCennikow.Add(c);
+        }
+        public List<Cennik> OdajListe()
+        {
+            return listaCennikow;
+        }
+    }
+}
