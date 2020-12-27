b944c2a12a6088cc215f0170c73fc19c1b281e2c
holygwent <39002523+holygwent@users.noreply.github.com>
2020-12-27 11:52:57 +0100

    Decorator_test

---
+++ IOApplication/Test_darek/TestDecorator.cs
@@ -0,0 +1,30 @@
+﻿using IOApplication.Models.CennikModel.Component;
+using IOApplication.Models.CennikModel.ConcreteComponent;
+using IOApplication.Models.CennikModel.ConcreteKarnetDecorator;
+using IOApplication.Models.CennikModel.ConcreteZajeciaDecorator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test_darek
+{
+    [TestClass]
+    public class TestDecorator
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //arrange
+            int kosz = 85;
+            string nazwa = "Karnet:180-dniowy wraz z zajęciami: Joga, Boks, Fitness,";
+            //act
+            Cennik promocja = new ConcreteComponent();
+            promocja = new Days180KarnetDecorator(promocja);
+            promocja = new JogaDecorator(promocja);
+            promocja = new BoksDecorator(promocja);
+            promocja = new FitnessDecorator(promocja);
+            //assert
+            Assert.AreEqual(promocja.GetName(), nazwa);
+            Assert.AreEqual(promocja.CalculateCost(), kosz);
+        }
+    }
+}
