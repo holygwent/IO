646c543d64abf7df37e62c0adfbe66d5724d0c4a
holygwent <39002523+holygwent@users.noreply.github.com>
2021-01-12 18:29:32 +0100

    simple facotry enum

---
+++ IOApplication/IOApplication/Content/Csharp/Fabryka/TrenerFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IOApplication.Content.Csharp.Fabryka
+{
+    public enum TrenerPoID
+    {
+        Antek_Bury = 1,
+        Andrzej_Kwitek
+    }
+}
