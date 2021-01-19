1402a3e8a96a8506a3085134353a11e33f242940
holygwent <39002523+holygwent@users.noreply.github.com>
2021-01-19 20:16:14 +0100

    bug naprawiony

--- IOApplication/IOApplication/Models/Silownia2.Designer.cs
+++ IOApplication/IOApplication/Models/Silownia2.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Mateusz\Desktop\IO\IOApplication\IOApplication\Models\Silownia2.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\48502\source\repos\IO_cala_appka_main\IOApplication\IOApplication\Models\Silownia2.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
