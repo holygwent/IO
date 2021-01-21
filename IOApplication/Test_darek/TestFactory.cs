aeda285dbf6391d460054db2240bff206b051a27
holygwent <39002523+holygwent@users.noreply.github.com>
2021-01-21 20:03:51 +0100

    test integracyjny

---
+++ IOApplication/Test_darek/TestFactory.cs
@@ -0,0 +1,32 @@
+﻿using IOApplication.Content.Csharp.Fabryka;
+using IOApplication.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test_darek
+{
+    [TestClass]
+    public class TestFactory
+    {
+        private SiłowniaEntities2 db = new SiłowniaEntities2();
+        [TestMethod]
+        
+        public void Sprawdzanie_poprawnosci_danych_trenera_z_fabryki()
+        {
+            //Arange
+            TrenerFactory fabryka = new TrenerFactory();
+            string imie="Andrzej", nazwisko="Kwitek";
+
+
+            //Act
+            var TrenerMiesiaca = fabryka.CreatePracownik(TrenerPoID.Andrzej_Kwitek);
+            //Assert
+            Assert.AreEqual(imie,TrenerMiesiaca.Imie);
+            Assert.AreEqual(nazwisko,TrenerMiesiaca.Nazwisko);
+
+
+        }
+       
+        
+    }
+}
